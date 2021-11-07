ba22fd234e037d0855967b7e3b5a31a54d6ee83d
Ahmadul Hasan <ahmadulhasan@ahmaduls-mbp.home>
2021-11-07 14:22:10 -0500

    11072021 2:21pm

---
+++ src/practice/NamingConventions.java
@@ -0,0 +1,25 @@
+package practice;
+/*
+ 1) Projects name generally starts with UpperCase
+ 2) Package name generally starts with LowerCase
+ 3) Class name starts with UpperCase
+ 4) Class names are written in UpperCamelCase
+ 5) Class names are usually noun or noun phrases.
+ */
+public class NamingConventions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+// Camel case example for a Project -- October2021CoreJava
+// Camel case example for a Package -- lec01JavaBasics
+// Camel case example for a class -- NamingConventions
+
+// What is the alternative of camel case?
+// Alternative of camel case example for a Project -- October_2021_core_java
+// Alternative of camel case example for a Package -- lec01_java_basics
+// Alternative of camel case example for a class -- Naming_conventions
