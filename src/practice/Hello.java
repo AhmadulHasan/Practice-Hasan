6787579026ce35e0e4ff03c25b28715bb1e87996
Ahmadul Hasan <ahmadulhasan@ahmaduls-mbp.home>
2021-11-07 14:32:10 -0500

    11072021 02:32pm

--- src/practice/Hello.java
+++ src/practice/Hello.java
@@ -2,8 +2,9 @@ package practice;
 
 public class Hello {
 
-public static void main(String[] args) {
-	System.out.println("Hello World!");
-System.out.println();
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is Hasan");
 
-}}
+	}
+}
